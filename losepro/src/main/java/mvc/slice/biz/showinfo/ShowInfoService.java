5a3898996bffa935d205d5027502bc0a13e03261
imrcrab <imrcrab@163.com>
2018-02-01 07:57:47 +0800

    删除无用注释，准备logback日志--1

--- losepro/src/main/java/mvc/slice/biz/showinfo/ShowInfoService.java
+++ losepro/src/main/java/mvc/slice/biz/showinfo/ShowInfoService.java
@@ -8,36 +8,21 @@ import mvc.slice.pojo.BlogTypeInfo;
 import java.util.List;
 
 public interface ShowInfoService {
-    /**
-     * @param aid
-     * @return
-     */
+
     BlogArticleInfo findBlogInfoDetails(String aid);
 
-    /**
-     * @return
-     */
+
     List<BlogBriefInfo> findAllInfo(PageInfoBean pageInfoBean);
 
-    /**
-     * @return
-     */
+
     List<BlogTypeInfo> findAllArticleType();
 
-    /**
-     * @param artType
-     * @return
-     */
+
     List<BlogBriefInfo> findOneTypeInfo(String artType);
 
-    /**
-     * @return
-     */
+
     PageInfoBean findPageInfos(PageInfoBean pageInfoBean);
 
-    /**
-     *
-     * @param countNumberMain
-     */
+
     List<BlogBriefInfo> findBriefInfo(short countNumberMain);
 }
