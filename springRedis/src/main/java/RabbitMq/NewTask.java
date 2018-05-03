1ef67b4558af80edccad48840e1c97c5ab7b96a3
crab <imrcrab@163.com>
2018-05-03 12:10:56 +0800

    test rabbitmq常见的几种模式

--- springRedis/src/main/java/RabbitMq/NewTask.java
+++ springRedis/src/main/java/RabbitMq/NewTask.java
@@ -1,29 +1,61 @@
 package RabbitMq;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.concurrent.TimeoutException;
-
+@Configuration()
 public class NewTask {
-    private static final String QUENUE_NAME = "workqueue";
+    private static final String QUENUE_NAME = "gg";
 
     public static void main(String[] args) {
+        test2();
+    }
+
+
+
+    /**
+     * 发布订阅模式
+     */
+    public static void test2() {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("127.0.0.1");
         try {
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
-            channel.queueDeclare(QUENUE_NAME, false, false, false, null);
+            channel.exchangeDeclare(QUENUE_NAME, "fanout");
+            String message = new Date().toString() + ":long something ----";
+            channel.basicPublish(QUENUE_NAME, "", null, message.getBytes());
+            System.out.println("[x] send message:" + message);
+
+            channel.close();
+            connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 工作队列模式
+     */
+    public void test1() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUENUE_NAME, true, false, false, null);
             for (int i = 0; i < 10; ++i) {
                 String dots = "";
                 for (int j = 0; j < i; ++j) {
                     dots += ".";
                 }
                 String message = "hello world" + dots + dots.length();
-                channel.basicPublish("",QUENUE_NAME,null,message.getBytes());
+                channel.basicPublish("", QUENUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
             }
             channel.close();
             connection.close();
--- springRedis/src/main/java/RabbitMq/Work.java
+++ springRedis/src/main/java/RabbitMq/Work.java
@@ -3,18 +3,100 @@ package RabbitMq;
 import com.rabbitmq.client.*;
 
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class Work {
-    private final  static String QUEUE_NAME="workqueue";
+    private final static String QUEUE_NAME = "gg";
 
     public static void main(String[] args) {
+        test3();
+    }
+
+
+    /**
+     * 发布订阅
+     */
+    public static void test3() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.exchangeDeclare(QUEUE_NAME, "fanout");
+            String queueName = channel.queueDeclare().getQueue();
+
+            channel.queueBind(queueName, QUEUE_NAME, "");
+            channel.basicConsume(queueName, true, new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    String message = new String(body, "UTF-8");
+                    System.out.println("[x]receiverd message:" + message);
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 工作队列
+     */
+    public void test2() {
         int hashCode = Work.class.hashCode();
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("127.0.0.1");
         try {
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
-            channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+            //公平分发
+            channel.basicQos(1);
+            final Consumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    String message = new String(body, "UTF-8");
+                    System.out.println("[x] received:" + message);
+                    try {
+                        doWork(message);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        System.out.println("[x] done");
+                        //手动发送一次应答反馈
+                        /*channel.basicAck(envelope.getDeliveryTag(), false);*/
+                    }
+                }
+            };
+            //打开应答机制
+            boolean autoAck = true;
+            channel.basicConsume(QUEUE_NAME, autoAck, consumer);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                System.out.println("----------");
+            }
+        }
+    }
+
+    public void test1() {
+        int hashCode = Work.class.hashCode();
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
        /*     channel.basicConsume(QUEUE_NAME,false,"",new DefaultConsumer(channel){
                 @Override
@@ -23,13 +105,13 @@ public class Work {
                 }
             });*/
 
-       QueueingConsumer consumer = new QueueingConsumer(channel);
-       channel.basicConsume(QUEUE_NAME,true,consumer);
-       while (true){
-           QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-           String message = new String(delivery.getBody());
-           System.out.println("[x] received"+message);
-       }
+            QueueingConsumer consumer = new QueueingConsumer(channel);
+            channel.basicConsume(QUEUE_NAME, true, consumer);
+            while (true) {
+                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+                String message = new String(delivery.getBody());
+                System.out.println("[x] received" + message);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
