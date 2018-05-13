eb7020b14076240fef33b63ea4c76579e692fb5c
crab <imrcrab@163.com>
2018-05-13 22:17:10 +0800

    add cros访问跨域配置

--- combinerabbit/src/main/java/com/example/combinerabbit/config/WebConfig.java
+++ combinerabbit/src/main/java/com/example/combinerabbit/config/WebConfig.java
@@ -3,14 +3,27 @@ package com.example.combinerabbit.config;
 import com.example.combinerabbit.config.aop.ControllerIntecpter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 配置详见博客
+ * https://my.oschina.net/wangnian/blog/689020
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor((HandlerInterceptor) new ControllerIntecpter()).addPathPatterns("/**");
 
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
     }
 }
