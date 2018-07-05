2d0be901ffc98af731cc98969fa5a3187d3a8333
imrcrab <imrcrab@163.com>
2018-07-05 23:58:50 +0800

    对于finalize的理解

--- rabbimtmq-redis/combinerabbit/src/main/java/com/example/combinerabbit/bs/StaticLearn.java
+++ rabbimtmq-redis/combinerabbit/src/main/java/com/example/combinerabbit/bs/StaticLearn.java
@@ -8,14 +8,41 @@ import java.util.Date;
 public class StaticLearn {
     private static Date date = new Date();
 
+    public static StaticLearn SAVE_HOOK = null;
+
     public static void main(String[] args) throws InterruptedException {
         /**
          * do not this
          * 日期不可变
          */
-        while (true) {
-            System.out.println(date);
-            Thread.sleep(3000);
+//        while (true) {
+//            System.out.println(date);
+//            Thread.sleep(3000);
+//        }
+        SAVE_HOOK = new StaticLearn();
+        SAVE_HOOK = null;
+        System.gc();
+        Thread.sleep(500);
+        if (null != SAVE_HOOK) { //此时对象应该处于(reachable, finalized)状态
+            System.out.println("Yes , I am still alive1");
+        } else {
+            System.out.println("No , I am dead");
+        }
+        SAVE_HOOK = null;
+        System.gc();
+        Thread.sleep(500);
+        if (null != SAVE_HOOK) {
+            System.out.println("Yes , I am still alive2");
+        } else {
+            System.out.println("No , I am dead2");
         }
     }
+
+    @Override
+    protected void finalize() throws Throwable {
+
+        super.finalize();
+        System.out.println("execute method finalize");
+        SAVE_HOOK = this;
+}
 }
